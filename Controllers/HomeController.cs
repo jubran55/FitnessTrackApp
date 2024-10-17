86f5325f939b394b227abe2b52240dae2d81b9e8
jubran alyaf3i <jubran_alyaf3i@outlook.com>
2024-10-17 14:20:30 +0300

    الدروب لست شغاله والحسابات صحيحة

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,19 +1,17 @@
-using FitnessTrackApp.Models;
+﻿using FitnessTrackApp.Models;
 using FitnessTrackApp.ViewModels;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace FitnessTrackApp.Controllers
 {
-    //Scaffold-DbContext "Server=DESKTOP-USI4BRO;Database=FitnessTrackDB;Trusted_Connection=True;Trust Server Certificate=true" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models -Force  -DataAnnotations
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
         private readonly FitnessTrackDbContext _dbContext;
 
-        public HomeController(ILogger<HomeController> logger , FitnessTrackDbContext dbContext)
+        public HomeController(ILogger<HomeController> logger, FitnessTrackDbContext dbContext)
         {
             _logger = logger;
             _dbContext = dbContext;
@@ -21,7 +19,6 @@ namespace FitnessTrackApp.Controllers
 
         public IActionResult Index()
         {
-
             var FoodTypeList = _dbContext.MtfoodTypes.ToList();
             var FoodNutritionsList = _dbContext.MtfoodNutritions.ToList();
 
@@ -39,17 +36,40 @@ namespace FitnessTrackApp.Controllers
             return View();
         }
 
+        public JsonResult GetFoodItemsByType(int foodTypeId)
+        {
+            var foodItems = _dbContext.MtfoodNutritions
+                .Where(f => f.FoodTypeId == foodTypeId).ToList();
+
+            return Json(foodItems);
+        }
+
+        [HttpPost]
+        public IActionResult CalculateNutrition(int foodId, int weight)
+        {
+            var foodItem = _dbContext.MtfoodNutritions.FirstOrDefault(f => f.FoodId == foodId);
+
+            if (foodItem == null)
+            {
+                return NotFound();
+            }
+
+            var nutrition = new
+            {
+                Calories = (foodItem.Calories * weight) / 100,
+                Protein = (foodItem.Protein * weight) / 100,
+                Carbs = (foodItem.Carbohydrates * weight) / 100,
+                Fats = (foodItem.Fats * weight) / 100,
+                DietaryFiber = (foodItem.DietaryFiber * weight) / 100
+            };
+
+            return Json(nutrition);
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-
-        //public IActionResult GetFoodType()
-        //{
-            
-        //}
-
-
     }
 }
