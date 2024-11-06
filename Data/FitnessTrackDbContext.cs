22bef9e6c78a6000268a2bea60ab036102944f24
jubran alyaf3i <jubran_alyaf3i@outlook.com>
2024-11-06 22:32:24 +0300

    move to online

--- Data/FitnessTrackDbContext.cs
+++ Data/FitnessTrackDbContext.cs
@@ -24,7 +24,7 @@ public partial class FitnessTrackDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-USI4BRO;Database=FitnessTrackDB;Trusted_Connection=True;Trust Server Certificate=true");
+        => optionsBuilder.UseSqlServer("Server=db9783.public.databaseasp.net; Database=db9783; User Id=db9783; Password=8Mi=c#C7-E6g; Encrypt=False; MultipleActiveResultSets=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
---
+++ Migrations/20241106192546_mig1.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitnessTrackApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MTFoodType",
+                columns: table => new
+                {
+                    FoodTypeID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TypeNameEnglish = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    TypeNameArabic = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__MtFoodTy__516F03B50CA9EA02", x => x.FoodTypeID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MTFoodNutrition",
+                columns: table => new
+                {
+                    FoodID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FoodNameEnglish = table.Column<string>(type: "varchar(512)", unicode: false, maxLength: 512, nullable: true),
+                    FoodNameArabic = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    FoodTypeID = table.Column<int>(type: "int", nullable: true),
+                    weight100 = table.Column<int>(type: "int", nullable: true),
+                    Calories = table.Column<double>(type: "float", nullable: true),
+                    Protein = table.Column<double>(type: "float", nullable: true),
+                    Carbohydrates = table.Column<double>(type: "float", nullable: true),
+                    Fats = table.Column<double>(type: "float", nullable: true),
+                    DietaryFiber = table.Column<double>(type: "float", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__MtFoodNu__3214EC27EE34EC3B", x => x.FoodID);
+                    table.ForeignKey(
+                        name: "FK_MtFoodNutrition_MtFoodType",
+                        column: x => x.FoodTypeID,
+                        principalTable: "MTFoodType",
+                        principalColumn: "FoodTypeID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MTFoodNutrition_FoodTypeID",
+                table: "MTFoodNutrition",
+                column: "FoodTypeID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MTFoodNutrition");
+
+            migrationBuilder.DropTable(
+                name: "MTFoodType");
+        }
+    }
+}
